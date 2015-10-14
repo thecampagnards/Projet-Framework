38d6620fb6934d0a8fc90c1a3c69c75930340bb5
konstantin <konstantin.sidorenko@orange.fr>
2015-10-14 15:53:56 +0200

    ajout bootstrap

--- views/layout/default_layout.html.php
+++ views/layout/default_layout.html.php
@@ -2,7 +2,13 @@
 <html>
 <head>
 <title><?php echo $title; ?></title>
-<LINK rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 
