6f0ec25ef0c1dcdc179c283a9ac51638c732d742
konstantin <konstantin.sidorenko@orange.fr>
2015-10-24 11:42:29 +0200

    -ajout des modals -améliorations visuels

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 $(document).ready( function () {
 $('#table_id').dataTable( {
-      	"aoColumnDefs": [
-          { 'bSortable': false, 'aTargets': [ 0,5  ] }
-       ],
-	"order": [[ 1, "desc" ]]
+	"aoColumnDefs": [{ 'bSortable': false, 'aTargets': [ 0,5  ] }],
+	"order": [[ 1, "asc" ]],
+	"fnDrawCallback" : function(settings) {
+     	 $("#nb_lignes").html(' /Liste de '+settings.aoData.length+' éléments');
+	},
+	'iDisplayLength': 10
 });
-} );
+
+// Listen for click on toggle checkbox
+$('#select-all').click(function(event) {   
+    if(this.checked) {
+        // Iterate each checkbox
+        $(':checkbox').each(function() {
+            this.checked = true;                        
+        });
+    }
+    else {
+    	$(':checkbox').each(function() {
+          this.checked = false;
+      	});
+ 	}
+});	
+});
--- views/documents.html.php
+++ views/documents.html.php
@@ -1,50 +1,94 @@
 <?php 
 require_once 'db.php';
-echo "<h1>$title</h1>";?> 
-
+echo '<h1>'.$title.'<small id="nb_lignes"></small></h1>';?> 
+<!--Alertes pour affichers les resultats des actions-->
 <div id="ban">
-	<div class="alert alert-success" role="alert">...</div>
-	<div class="alert alert-warning" role="alert">...</div>
-	<div class="alert alert-danger" role="alert">...</div>
+  <div class="alert alert-success">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <strong>Success!</strong> This alert box could indicate a successful or positive action.
+  </div>
+    <div class="alert alert-warning">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <strong>Warning!</strong> This alert box could indicate a successful or positive action.
+  </div>
+    <div class="alert alert-danger">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <strong>Danger!</strong> This alert box could indicate a successful or positive action.
+  </div>
 </div>
 
 <div id="buttons-tab">
 	<button type="button" class="btn btn-success">Ajouter</button>
 	<button type="button" class="btn btn-danger">Supprimer</button>
 </div>
-
+<!-- Tableau de données-->
 <table id="table_id" class="table table-striped table-hover display table-bordered">
     <thead>
-  <tr>
-    <th>#</th>
-    <th class="sorting">rang</th>
-    <th class="sorting">promo</th>
-    <th class="sorting">libelle</th>
-    <th class="sorting">fichier</th>
-    <th></th>
-  </tr>
+    <tr>
+      <th><input type="checkbox" name="select-all" id="select-all" /></th>
+      <th class="sorting">rang</th>
+      <th class="sorting">promo</th>
+      <th class="sorting">libelle</th>
+      <th class="sorting">fichier</th>
+      <th></th>
+    </tr>
     </thead>
-  <tbody>
-<?php $reponse = $bdd->query('SELECT * FROM document ORDER BY rang');
-while ($donnees = $reponse->fetch()){ ?>
-  <tr>
-    <td><input type="checkbox" id="inlineCheckbox1" value="<?php echo $donnees['id']; ?>"></td>
-    <td><?php echo $donnees['rang']; ?></td>
-    <td><?php echo $donnees['promo']; ?></td>
-    <td><?php echo $donnees['libelle']; ?></td>
-    <td><?php echo $donnees['fichier']; ?></td>
-    <td>
-	<button type="button" class="btn btn-default" aria-label="<?php echo $donnees['id']; ?>">
-	  <span class="glyphicon  glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span>
-	</button>
-        <button type="button" class="btn btn-default" aria-label="<?php echo $donnees['id']; ?>">
-	  <span class="glyphicon  glyphicon glyphicon-trash" aria-hidden="true"></span>
-	</button>
-    </td>
-  </tr>
-  <?php }
-  $reponse->closeCursor(); // Termine le traitement de la requête
-  ?>
-  </tbody>
-
+    <tbody>
+    <?php $reponse = $bdd->query('SELECT * FROM document ORDER BY rang');
+    while ($donnees = $reponse->fetch()){ ?>
+    <tr>
+      <td><input type="checkbox" value="<?php echo $donnees['id']; ?>"></td>
+      <td><?php echo $donnees['rang']; ?></td>
+      <td><?php echo $donnees['promo']; ?></td>
+      <td><?php echo $donnees['libelle']; ?></td>
+      <td><?php echo $donnees['fichier']; ?></td>
+      <td>
+      	<button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModalMod<?php echo $donnees['id']; ?>" aria-label="<?php echo $donnees['id']; ?>">
+      	  <span class="glyphicon  glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span>
+      	</button>
+        <button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModalDel<?php echo $donnees['id']; ?>" aria-label="<?php echo $donnees['id']; ?>">
+      	  <span class="glyphicon  glyphicon glyphicon-trash" aria-hidden="true"></span>
+      	</button>
+      </td>
+    </tr>
+    <!-- Modal Modifier-->
+    <div class="modal fade" id="myModalMod<?php echo $donnees['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Modifier le document <?php echo $donnees['libelle']; ?></h4>
+          </div>
+          <div class="modal-body">
+            <?php echo $donnees['libelle']; ?>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+            <button type="button" class="btn btn-primary">Enregistrer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Modal Supprimer-->
+    <div class="modal fade" id="myModalDel<?php echo $donnees['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Supprimer le document <?php echo $donnees['libelle']; ?></h4>
+          </div>
+          <div class="modal-body">
+            <?php echo $donnees['libelle']; ?>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+            <button type="button" class="btn btn-primary">Supprimer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php }
+    $reponse->closeCursor();
+    ?>
+    </tbody>
 </table> 
