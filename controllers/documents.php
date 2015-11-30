67e374ef56949754e5913abb7632eebc3516b6e4
thecampagnards <thecampagnards@gmail.com>
2015-11-30 11:57:52 +0100

    update du form document

--- controllers/documents.php
+++ controllers/documents.php
@@ -5,12 +5,6 @@ function documents_controller(){
 	//si post
 	if(!empty($_POST)) {
 
-			echo '<pre>';
-
-		var_dump($_POST);
-		echo '<br/>';
-		var_dump($_FILES);
-
   		if($_POST['action'] == 'edit') $SUCCESS = editDocument($_POST['id'], $_POST['rang'], $_POST['promo'], $_POST['libelle'], $_FILES['fichier']);
   		elseif($_POST['action'] == 'add') $SUCCESS = addDocument($_POST['rang'], $_POST['promo'], $_POST['libelle'], $_FILES['fichier']);
   		elseif($_POST['action'] == 'delete') $SUCCESS = deleteDocument($_POST['id']);
@@ -20,5 +14,7 @@ function documents_controller(){
 	set("title", "Documents");
 	set("subtitle", 'Il y a '.count($documents).' document(s)');
 	set("documents", $documents);
+	set("SUCCESS", $SUCCESS);
+
 	return html('documents.html.php','layout/default_layout.html.php');
 }
--- modeles/documents.php
+++ modeles/documents.php
@@ -2,11 +2,11 @@
 
 //fonction pour récupérer les documents
 function getDocuments() {
-  $bdd = connectDB();
-  $sql = 'SELECT * FROM document ORDER BY rang';
-  $res = $bdd->prepare($sql);
-  $res->execute();
-  return $res->fetchAll(PDO::FETCH_ASSOC);
+	$bdd = connectDB();
+	$sql = 'SELECT * FROM document ORDER BY rang';
+	$res = $bdd->prepare($sql);
+	$res->execute();
+	return $res->fetchAll(PDO::FETCH_ASSOC);
 }
 
 //fonction pour editer un document
@@ -40,16 +40,24 @@ function addDocument($rang, $promo, $libelle, $fichier) {
 
 	$sql = 'INSERT INTO document SET
 	rang = :rang,
+	promo = :promo,
 	libelle = :libelle,
 	fichier = :fichier';
 
 	$stmt = $bdd->prepare($sql);
 
+	if(checkPromotion($promo) == 1){
+		$fichier_db = 'A12/'.$fichier['name'];
+	}else if(checkPromotion($promo) == 2){
+		$fichier_db = 'A345/'.$fichier['name'];
+	}else{
+		$fichier_db = $fichier['name'];
+	}
+
 	$stmt->bindParam(':rang', $rang, PDO::PARAM_INT);
 	$stmt->bindParam(':promo', $promo, PDO::PARAM_STR);
 	$stmt->bindParam(':libelle', $libelle, PDO::PARAM_STR);
-	$stmt->bindParam(':fichier', $fichier['name'], PDO::PARAM_STR);
-	var_dump($stmt);
+	$stmt->bindParam(':fichier', $fichier_db, PDO::PARAM_STR);
 
 	if($stmt->execute() or die(var_dump($stmt->ErrorInfo()))) {
 		return true;
