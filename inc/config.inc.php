69afbbaff0b8b4ba2dd8b9e4386fef8538f246bd
christophe <christophe.hardy@isen-bretagne.fr>
2015-12-10 10:23:18 +0100

    cece

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -2,7 +2,7 @@
 // log SQL
 define('SQL_HOST', 'localhost');
 define('SQL_USER', 'root');
-define('SQL_PASS', 's3curit3');
+define('SQL_PASS', 'christophe');
 define('SQL_DATABASE', 'projet_limonade');
 //dossier des fichiers
 define('FILES_DIR', './');
